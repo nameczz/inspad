4c11af138ec05f068df12eff18c2a6a893573fd3
zhangya <zhangya5847@163.com>
2018-05-16 16:20:53 +0800

    add components

---
+++ src/components/busi/Copyright.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="copyright" :class="position">Copyright © 2018 Patsnap. All rights reserved</div>
+</template>
+
+<script>
+export default {
+  props: {
+    position: {
+      type: String,
+    },
+  },
+}
+</script>
+
+<style scoped lang="scss">
+  @import "~sty/var";
+  .copyright{
+    height: 80px;
+    background-color: #133c44;
+    line-height: 80px;
+    text-align: center;
+    color: #000000;
+    font-size: 18px;
+    color: rgba(255,255,255,0.5);
+
+    &.absolute{
+      position: absolute;
+      left: 0;
+      width: 100%;
+      bottom: 0;
+    }
+  }
+</style>
